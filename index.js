6043d8c543944f105ea7cfb1bf5a5584a00863fc
Roei <Roeit1977@gmail.com>
2026-09-12 18:34:11 +0300

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
-const express = require("express")
 const app = express()
+const express = require("express")
 const fs = require("fs")
 const path = require("path")
 require("dotenv").config();
--- sku-table-manager.js
+++ sku-table-manager.js
@@ -1,4 +1,6 @@
 const db = require("./db_connection.js");
+const cors = require("cors");
+const app = express()
 
 const skurouter = require("express").Router();
 
@@ -6,6 +8,16 @@ const skurouter = require("express").Router();
 // ============================================================
 // GET SKU STRUCTURE
 // ============================================================
+
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://placebo-plm-js.vercel.app",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
 
 skurouter.get("/", async (req, res) => {
 
